f73f32e97e7cbc2acd7615284bc2248570f22f97
Guilherme Alves <guilherme19961985@hotmail.com>
2023-04-08 12:09:44 -0300

    first commit

---
+++ config/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Alura\Mvc\Controller\DeleteVideoController;
+use Alura\Mvc\Controller\EditVideoController;
+use Alura\Mvc\Controller\VideoFormController;
+use Alura\Mvc\Controller\VideoListController;
+use Alura\Mvc\Controller\NewVideoController;
+
+return [
+    'GET|/' => VideoListController::class,
+    'GET|/novo-video' => VideoFormController::class,
+    'POST|/novo-video' => NewVideoController::class,
+    'GET|/editar-video' => VideoFormController::class,
+    'POST|/editar-video' => EditVideoController::class,
+    'GET|/remover-video' => DeleteVideoController::class,
+
+];
